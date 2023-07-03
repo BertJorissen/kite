8ae15548bcf9e49bdd25e7056900f48efa1b5683
Bert Jorissen <bert.jorissen@uantwerpen.be>
2023-07-03 17:57:20 +0200

    print all

--- cppcore/cmake/copy.cmake
+++ cppcore/cmake/copy.cmake
@@ -1,9 +1,9 @@
 if(${HDF5_MOVE_FILES} STREQUAL "TRUE")
     # Find files matching the pattern "libhdf5.so*"
-    file(GLOB HDF5_LIBRARY_FILES "${HDF5_DIR}/build/bin/libhdf5.so*")
+    file(GLOB HDF5_LIBRARY_FILES "${HDF5_DIR}/build/libhdf5.so*")
 
     # Find files matching the pattern "libhdf5_cpp.so*"
-    file(GLOB HDF5_CPP_LIBRARY_FILES "${HDF5_DIR}/build/bin/libhdf5_cpp.so*")
+    file(GLOB HDF5_CPP_LIBRARY_FILES "${HDF5_DIR}/build/libhdf5_cpp.so*")
 
     # Concatenate the file lists
     set(ALL_LIBRARY_FILES ${HDF5_LIBRARY_FILES} ${HDF5_CPP_LIBRARY_FILES})
