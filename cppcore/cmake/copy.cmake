fbc429e0e753b225450c8ea24424773013c311a8
Bert Jorissen <bert.jorissen@uantwerpen.be>
2023-07-03 04:58:03 +0200

    add file name extentions

--- cppcore/cmake/copy.cmake
+++ cppcore/cmake/copy.cmake
@@ -1,9 +1,9 @@
 if(${HDF5_MOVE_FILES} STREQUAL "TRUE")
     # Find files matching the pattern "libhdf5.so*"
-    file(GLOB HDF5_LIBRARY_FILES "${HDF5_DIR}/build/bin/libhdf5.*")
+    file(GLOB HDF5_LIBRARY_FILES "${HDF5_DIR}/build/bin/libhdf5.*${HDF_SL_EXT}*")
 
     # Find files matching the pattern "libhdf5_cpp.so*"
-    file(GLOB HDF5_CPP_LIBRARY_FILES "${HDF5_DIR}/build/bin/libhdf5_cpp${HDF_SL_EXT}*")
+    file(GLOB HDF5_CPP_LIBRARY_FILES "${HDF5_DIR}/build/bin/libhdf5_cpp.*${HDF_SL_EXT}*")
 
     # Concatenate the file lists
     set(ALL_LIBRARY_FILES ${HDF5_LIBRARY_FILES} ${HDF5_CPP_LIBRARY_FILES})
@@ -11,5 +11,8 @@ if(${HDF5_MOVE_FILES} STREQUAL "TRUE")
     # Copy the files to the destination directory
     file(COPY ${ALL_LIBRARY_FILES} DESTINATION "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}")
 
-    MESSAGE(${ALL_LIBRARY_FILES}-${HDF5_DIR}-${CMAKE_LIBRARY_OUTPUT_DIRECTORY}-HDF5_MOVE_FILES)
+    # see if moved
+    file(GLOB moved "${HDF5_DIR}/build/bin/*")
+
+    MESSAGE(${ALL_LIBRARY_FILES}-${HDF5_DIR}-${CMAKE_LIBRARY_OUTPUT_DIRECTORY}-${moved}-${HDF_SL_EXT}-HDF5_MOVE_FILES)
 endif()
