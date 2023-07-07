b92f28d6f73e10b0de019743f64599f91c2da14a
Bert Jorissen <bert.jorissen@uantwerpen.be>
2023-07-07 11:51:04 +0200

    see name of compiler

--- cppcore/cmake/copy.cmake
+++ cppcore/cmake/copy.cmake
@@ -1,18 +1,6 @@
-if(${HDF5_MOVE_FILES} STREQUAL "TRUE")
-    # Find files matching the pattern "libhdf5.so*"
-    file(GLOB HDF5_LIBRARY_FILES "${HDF5_DIR}/build/bin/libhdf5.*${HDF_SL_EXT}*")
-
-    # Find files matching the pattern "libhdf5_cpp.so*"
-    file(GLOB HDF5_CPP_LIBRARY_FILES "${HDF5_DIR}/build/bin/libhdf5_cpp.*${HDF_SL_EXT}*")
-
-    # Concatenate the file lists
-    set(ALL_LIBRARY_FILES ${HDF5_LIBRARY_FILES} ${HDF5_CPP_LIBRARY_FILES})
-
-    # Copy the files to the destination directory
-    file(COPY ${ALL_LIBRARY_FILES} DESTINATION "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}")
-
-    # see if moved
-    file(GLOB moved "${HDF5_DIR}/build/bin/*")
-
-    MESSAGE(${ALL_LIBRARY_FILES}-${HDF5_DIR}-${CMAKE_LIBRARY_OUTPUT_DIRECTORY}-${moved}-${HDF_SL_EXT}-HDF5_MOVE_FILES)
+if(${EXECUTE_MOVE} STREQUAL "TRUE")
+    #files to be moves
+    file(GLOB FILES_TO_MOVE ${FILES_WITH_WILDCARD})
+    file(COPY ${FILES_TO_MOVE} DESTINATION ${FILES_MOVE_DESTINATION})
+    MESSAGE(-- COPIED ${FILES_TO_MOVE} FROM ${FILES_WITH_WILDCARD} TO ${FILES_MOVE_DESTINATION})
 endif()
--- setup.py
+++ setup.py
@@ -30,9 +30,7 @@ class CMakeBuild(build_ext):
 
         cmake_args += ["-DQK_NATIVE_HDF5=" + os.environ.get("QK_NATIVE_HDF5", "OFF"),
                        "-DQK_NATIVE_EIGEN=" + os.environ.get("QK_NATIVE_EIGEN", "OFF"),
-                       "-DOSX_FLAGS=" + os.environ.get("OSX_FLAGS", " -arch x86_64 -O3"),
-                       "-DOSX_CPP_FLAGS=" + os.environ.get("OSX_CPP_FLAGS", " "),
-                       "-DOSX_ARCH=" + os.environ.get("OSX_ARCH", "x86_64")]
+                       "-DQK_CCACHE=" + os.environ.get("QK_CCACHE", "OFF")]
         try:
             import h5py
             cmake_args += ["-DHDF5_DOWNLOAD_VERSION=" + os.environ.get("HDF5_DOWNLOAD_VERSION", h5py.version.hdf5_version)]
